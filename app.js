6a7ba5449068f1ecb532ce48301d9a68399c2469
Brandt Strom <brandtkstrom@gmail.com>
2019-07-12 18:29:55 -0700

    add time picker

--- app.js
+++ app.js
@@ -12,3 +12,29 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 const database = firebase.database();
+
+const timepicker = new TimePicker(["input-train-time"], {
+    theme: "dark",
+    lang: "en"
+});
+timepicker.on("change", evt => {
+    console.log(evt);
+
+    let value = `${evt.hour || "00"}:${evt.minute || "00"}`;
+    evt.element.value = value;
+});
+
+class TrainScheduleItem {
+    constructor(name, dest, timeFirst, frequency) {
+        this.name = name;
+        this.destination = dest;
+        this.firstTrainTime = timeFirst;
+        this.frequency = frequency;
+        this.nextArrival = null;
+        this.minutesAway = null;
+    }
+
+    calcNextArrival() {
+        // TODO
+    }
+}
